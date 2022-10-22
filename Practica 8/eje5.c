6d89d16d2e5f77f92fd2235d1fe746710e5f170f
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2022-10-22 11:07:26 -0300

    Add files via upload

---
+++ Practica 8/eje5.c
@@ -0,0 +1,25 @@
+// Programa que muestra un ejemplo de como producir BASURA con listas dinamicas
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Nodo {
+	int info;
+	struct Nodo * next;
+};
+
+struct Nodo * p = NULL; // inicializo el puntero p en NULL
+
+int main (){
+	
+	p = (struct Nodo *) malloc (sizeof(struct Nodo));
+	p->info = 10;
+	p->next = NULL;
+	
+	struct Nodo * q = NULL; // inicializo al puntero q en null
+	q = p;
+	
+	free (q); // se libera el puntero q y automaticamente tambien se libera el puntero p
+	
+	printf ("Valor basura: %d", q->info); // se muestra basura, tambien puede hacerse: p->info
+	
+}
