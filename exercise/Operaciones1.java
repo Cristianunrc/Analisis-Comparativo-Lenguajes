bc72081364a4852128da7724196f26b2526ea84c
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2023-02-27 11:33:52 -0300

    Clase Operaciones que extiende la clase Operaciones1

--- exercise/Operaciones1.java
+++ exercise/Operaciones1.java
@@ -3,7 +3,7 @@ package exercise;
 
 /**
  * Clase Operaciones1 que extiende (hereda) la clase Operaciones e implementa sus metodos.
- * La clase abstracta Operaciones es una superclase de Operaciones1 y a su vez esta es una subclase.
+ * La clase abstracta Operaciones es una superclase de Operaciones1 y a su vez ésta es una subclase.
  * @author pc
  *
  */
